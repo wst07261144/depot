a4915a459579a76487ce384e6b3d4407d417bb44
wst07261144 <wst07261144@gmail.com>
2014-03-14 22:41:50 +0800

    product

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,8 @@
 class Product < ActiveRecord::Base
+  validate :title, :description, :image_url, :presence=>true
+  validate :price, :numericality => {:greater_than_equal_to => 0.01}
+  validate :image_url, :format=>{
+      :with => %r{\.(gif|jpg|png)$}i,
+      :message => '必须是GIF、JPG、或者PNG类型的URL'
+  }
 end
