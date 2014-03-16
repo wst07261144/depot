8e28eccfa1998490438a7e8f5e9d5ce77780312a
wst07261144 <wst07261144@gmail.com>
2014-03-16 19:40:08 +0800

    product index page add color and sort and image url

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,10 +1,14 @@
 class Product < ActiveRecord::Base
 
-  default_scope :order=>'title'
+  default_scope :order => 'title'
 
   validates :title, presence: true
-  validates :description, presence:true
-  validates :image_url, presence:true ,uniqueness: true
+  validates :description, presence: true
+  validates :image_url, presence: true, uniqueness: true
+  validates :image_url1, uniqueness: true
+  validates :image_url2, uniqueness: true
+  validates :image_url3, uniqueness: true
+  validates :image_url4, uniqueness: true
   validates :price, :numericality => {:greater_than_equal_to => 0.01}
   #validates_format_of :image_url,
   #    :with=> %r{\A/*(gif|jpg|png)\z}i,
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140316082227) do
+ActiveRecord::Schema.define(version: 20140316113028) do
 
   create_table "image_urls", force: true do |t|
     t.integer  "product_id"
@@ -34,6 +34,12 @@ ActiveRecord::Schema.define(version: 20140316082227) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.decimal  "price",       precision: 8, scale: 2, default: 0.0
+    t.string   "sort"
+    t.string   "color"
+    t.string   "image_url1"
+    t.string   "image_url2"
+    t.string   "image_url3"
+    t.string   "image_url4"
   end
 
   create_table "users", force: true do |t|
